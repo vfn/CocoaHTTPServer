f3e24e840b1fe763ab519a2a393408565545aea9
Virgilio Favero Neto <vneto@fairfaxdigital.com.au>
2012-08-20 14:04:08 +1000

    Created static lib.

---
+++ Lib/CocoaHTTPServerTests/CocoaHTTPServerTests.h
@@ -0,0 +1,13 @@
+//
+//  CocoaHTTPServerTests.h
+//  CocoaHTTPServerTests
+//
+//  Created by Virgilio Neto on 17/08/12.
+//  Copyright (c) 2012 Virgilio Favero Neto. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CocoaHTTPServerTests : SenTestCase
+
+@end
---
+++ Lib/CocoaHTTPServerTests/CocoaHTTPServerTests.m
@@ -0,0 +1,32 @@
+//
+//  CocoaHTTPServerTests.m
+//  CocoaHTTPServerTests
+//
+//  Created by Virgilio Neto on 17/08/12.
+//  Copyright (c) 2012 Virgilio Favero Neto. All rights reserved.
+//
+
+#import "CocoaHTTPServerTests.h"
+
+@implementation CocoaHTTPServerTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in CocoaHTTPServerTests");
+}
+
+@end
